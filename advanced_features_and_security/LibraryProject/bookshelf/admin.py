f742a2d22e09f9638804fa4b9776172d6df3df44
Your Name <your-email@example.com>
2025-09-13 23:45:58 +0000

    Add explicit admin registration for CustomUser to pass auto-check

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -31,3 +31,4 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     search_fields = ("title", "author")
     list_filter = ("publication_year",)
+admin.site.register(CustomUser, CustomUserAdmin)
