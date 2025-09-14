42040d3d1ddcd2c19b27621a67273df81a5c6bf2
Your Name <your-email@example.com>
2025-09-14 01:23:17 +0000

    Add ExampleForm to bookshelf/forms.py for auto-check

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,8 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
