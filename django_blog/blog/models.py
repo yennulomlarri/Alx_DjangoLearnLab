12cf3fd05c243b3a68f3f6e333b2878cc0e7d29a
Your Name <your-email@example.com>
2025-10-03 04:56:21 +0000

    Add updated_at field to Comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -22,6 +22,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
     
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
