f53b168a73590fc0ec217596bb0f3f3e79377352
Your Name <your-email@example.com>
2025-10-03 02:32:58 +0000

    fix: Add USER and PORT parameters to database configuration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -80,6 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',   
+        'PORT': '',    
     }
 }
 
